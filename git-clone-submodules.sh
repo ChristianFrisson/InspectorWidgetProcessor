81868982059c6a1732cf83d81e676cc329c6cd6f
Christian Frisson <christian.frisson@gmail.com>
2016-02-26 18:06:20 +0100

    npm: preinstall git submodules by cloning them (hack)

---
+++ git-clone-submodules.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone --depth=1 https://github.com/InspectorWidget/libuiohook.git 3rdparty/github-InspectorWidget-libuiohook
+git clone --depth=1 https://github.com/InspectorWidget/PartialCsvParser.git 3rdparty/github-InspectorWidget-PartialCsvParser
+git clone --depth=1 https://github.com/InspectorWidget/PEGTL.git 3rdparty/github-InspectorWidget-PEGTL
+git clone --depth=1 https://github.com/InspectorWidget/rapidjson.git 3rdparty/github-InspectorWidget-rapidjson
