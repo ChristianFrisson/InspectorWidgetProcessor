7e69b23d16815f8c0e7e295399128b9e73591a71
Christian Frisson <christian.frisson@gmail.com>
2016-03-01 23:30:50 +0100

    npm: improve git submodules preinstall by cloning if directories are empty

--- git-clone-submodules.sh
+++ git-clone-submodules.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-git clone --depth=1 https://github.com/InspectorWidget/libuiohook.git 3rdparty/github-InspectorWidget-libuiohook
-git clone --depth=1 https://github.com/InspectorWidget/PartialCsvParser.git 3rdparty/github-InspectorWidget-PartialCsvParser
-git clone --depth=1 https://github.com/InspectorWidget/PEGTL.git 3rdparty/github-InspectorWidget-PEGTL
-git clone --depth=1 https://github.com/InspectorWidget/rapidjson.git 3rdparty/github-InspectorWidget-rapidjson
+deps="libuiohook PartialCsvParser PEGTL rapidjson";
+for dep in $deps;
+do 
+	if [ "$(ls -A 3rdparty/github-InspectorWidget-$dep)" ]; then
+		echo "$dep already cloned"
+	else
+		echo "cloning $dep"
+		git clone --depth=1 https://github.com/InspectorWidget/$dep.git 3rdparty/github-InspectorWidget-$dep
+	fi
+done
